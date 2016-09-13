016112fc8cc1cb96cc253c418fd0953b05915b36
rbnkrgr <mail@robinkrueger.de>
2016-09-13 19:03:17 +0200

    Erstes Programm

---
+++ Bild_speichern.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Bild_speichern.py
+#  
+#  This program was created as part of the laboratory Mathesis at the Technical University Berlin .
+#  Copyright 2016 Henriette Behr, Henriette Rilling, Max Wehner, Robin Krueger <das_orchester_ist_programm@web.de>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+	cv2.imshow('frame',frame)
+ 
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	if cv2.waitKey(1) & 0xFF == ord('b'):
+		cv2.imwrite('Ausgabebild.png',frame)
+	
+# Stream und Fenster werden geschlossen
+cap.release()
+cv2.destroyAllWindows()
